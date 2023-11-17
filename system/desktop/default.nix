2d660aa3e4da4fc32e24317b7d841a8f4c9b49ac
tianze0926 <i@tianze.me>
2023-11-17 15:23:15 +0800

    refactor(conditional imports): a better workaround

--- system/desktop/default.nix
+++ system/desktop/default.nix
@@ -1,18 +1,23 @@
-args@{ lib, config, pkgs, ... }:
-with lib;
-let
-  cfg = config.custom.desktop;
-in {
-  options.custom.desktop.enable = mkEnableOption "Enable Desktop";
+{ lib, ... }: {
+  options.custom.desktop.enable = lib.mkEnableOption "Enable Desktop";
 
-  # a workaround for infinite recursion when conditionally importing modules
-  config = mkMerge (builtins.map (
-    path: mkIf cfg.enable (import path args)
-  ) [
+  # conditionally import modules based on config options
+  # while preventing infinite recursion
+  imports = let
+    condition = config: config.custom.desktop.enable;
+  in builtins.map (path: rec {
+    function = import path;
+    wrappedFunction = args@{ lib, config, ... }:
+      lib.mkIf (condition config) (function args);
+    # preserve the original function's args declaration
+    # so that the module system would pass lazy args such as `pkgs`
+    argsDeclaredFunction = lib.setFunctionArgs
+      wrappedFunction (lib.functionArgs function);
+  }.argsDeclaredFunction) [
     ./display.nix
     ./fonts.nix
     ./fcitx5.nix
     ./media.nix
-  ]);
+  ];
 
 }
