814ffc6d776aaf621e6b43cbfcce01aba04457cd
tianze0926 <i@tianze.me>
2023-11-18 23:42:48 +0800

    feat(locker): add swaylock

--- system/desktop/display.nix
+++ system/desktop/display.nix
@@ -1,19 +1,33 @@
-{ pkgs, ... }: {
-  programs.hyprland.enable = true;
-  hardware.opengl.driSupport32Bit = true;
-  hardware.opengl.extraPackages = with pkgs; [
-    vaapiVdpau
-    libvdpau-va-gl
-  ];
-  hardware.opengl.extraPackages32 = with pkgs; [
-    driversi686Linux.vaapiVdpau
-    driversi686Linux.libvdpau-va-gl
-  ];
-
-  services.ddccontrol.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    nvtop-amd
-  ];
-
-}
+{ ... }: { imports = [
+  ({ ... }: {
+    programs.hyprland.enable = true;
+    services.ddccontrol.enable = true;
+  })
+  ({ pkgs, ... }: let
+    pkg = pkgs.swaylock;
+    pam = "swaylock";
+  in {
+    environment.systemPackages = [ pkg ];
+    security.pam.services."${pam}".text =
+      builtins.readFile "${pkg}/etc/pam.d/${pam}";
+    hm = [({ ... }: {
+      home.file.".swaylock/config".text = ''
+        color=000000
+      '';
+    })];
+  })
+  ({ pkgs, ... }: {
+    hardware.opengl.driSupport32Bit = true;
+    hardware.opengl.extraPackages = with pkgs; [
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+    hardware.opengl.extraPackages32 = with pkgs; [
+      driversi686Linux.vaapiVdpau
+      driversi686Linux.libvdpau-va-gl
+    ];
+    environment.systemPackages = with pkgs; [
+      nvtop-amd
+    ];
+  })
+];}
