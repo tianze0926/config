0a0eea88b36afaafa7087b461b7e309ebcbb5ade
tianze0926 <i@tianze.me>
2023-11-21 17:06:18 +0800

    fix(mpv): lib issue launching mpv from firefox

--- home/desktop/mpv.nix
+++ home/desktop/mpv.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, osConfig, ... }: {
   programs.mpv = {
     enable = true;
     config = {
@@ -8,11 +8,25 @@
 
   # uri scheme
   xdg.mimeApps.defaultApplications."x-scheme-handler/mpv" = "mpv-uri.desktop";
-  xdg.desktopEntries.mpv-uri = {
+  xdg.desktopEntries.mpv-uri = let
+    script = pkgs.writeText "mpv-uri.sh" rec {
+      mapper = v: rec {
+        value = v.LD_LIBRARY_PATH or [];
+        res = if builtins.isList value then value else [ value ];
+      }.res;
+      libpath = lib.unique (builtins.concatLists (builtins.map mapper [
+        config.home.sessionVariables
+        osConfig.environment.variables
+        osConfig.environment.sessionVariables
+        osConfig.environment.profileRelativeSessionVariables
+      ]));
+      res = ''
+        export LD_LIBRARY_PATH=${builtins.concatStringsSep ":" libpath}
+        ${config.programs.mpv.package}/bin/mpv ''${1#mpv://}
+      '';
+    }.res;
+  in {
     name = "mpv for uri scheme";
-    exec = "${pkgs.bash}/bin/bash ${config.home.homeDirectory}/.config/mpv/mpv-uri.sh %u";
+    exec = "${pkgs.bash}/bin/bash ${script} %u";
   };
-  home.file.".config/mpv/mpv-uri.sh".text = ''
-    ${config.programs.mpv.package}/bin/mpv ''${1#mpv://}
-  '';
 }
