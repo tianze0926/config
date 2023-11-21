e67d6c8b2be560201a789bc5198299eed974a918
tianze0926 <i@tianze.me>
2023-11-21 20:17:28 +0800

    chore(typst): remove font patch
    Font discovery through fontconfig has been supported since Typst 0.9.0.

--- system/typst.nix
+++ system/typst.nix
@@ -7,8 +7,4 @@
     xdg.dataFile."typst".source = "${pkgs.myRepo.typst-svg-emoji}/share/typst";
   })];
 
-  fonts.fontDir.enable = true;
-  environment.variables = {
-    TYPST_FONT_PATHS = "/run/current-system/sw/share/X11/fonts";
-  };
 }
