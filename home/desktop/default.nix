5f4a25b1e485019088a4032df35614657b4c7a15
tianze0926 <i@tianze.me>
2024-01-15 22:42:03 +0800

    refactor: seperate hyprland config

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,51 +1,21 @@
-{ pkgs, ... }: let
-  cursor.package = pkgs.capitaine-cursors;
-  cursor.name = "capitaine-cursors";
-in {
+{ pkgs, ... }: {
   imports = [
-    ./hyprland.nix
+    ./hyprland
+
     ./terminal.nix
     ./mpv.nix
-    ./dunst.nix
     ./mimeapps
     ./pdf.nix
     ./wemeet
   ];
-  home.pointerCursor = {
-    gtk.enable = true;
-    # x11.enable = true;
-    inherit (cursor) package name;
-    size = 24;
-  };
-  gtk = {
-    enable = true;
-    cursorTheme = { inherit (cursor) package name; };
-    iconTheme.package = pkgs.gnome.adwaita-icon-theme;
-    iconTheme.name = "Adwaita";
-    theme.package = pkgs.gnome.gnome-themes-extra;
-    theme.name = "Adwaita";
-  };
-  qt.style.package = pkgs.adwaita-qt6;
-  qt.style.name = "Adwaita";
-
-  programs.waybar = {
-    enable = true;
-    settings.mainBar = import ./waybar/config.nix;
-    style = ./waybar/style.css;
-  };
 
   home.packages = with pkgs; [
-    xdg-utils xorg.xhost xorg.xeyes
-    pantheon.elementary-files
+    xorg.xeyes
     gparted
     firefox ungoogled-chromium
-    copyq
     libreoffice
     jellyfin-media-player
-    rofi-wayland grimblast
     font-manager
-    networkmanagerapplet
-    polkit_gnome
     vscodium-fhs
     inkscape-with-extensions
     wireshark
@@ -54,7 +24,6 @@ in {
     praat
     remmina
     audacity
-    ark
     cloudflared
   ];
 
