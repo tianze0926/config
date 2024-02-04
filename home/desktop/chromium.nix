508f6a2044a84eedb7b7c06d30bd20409b4ea62a
tianze0926 <i@tianze.me>
2024-02-04 22:52:36 +0800

    chore(chromium): different ime flag for gnome

--- home/desktop/chromium.nix
+++ home/desktop/chromium.nix
@@ -1,9 +1,12 @@
-{ pkgs, ... }: let
+{ pkgs, osConfig, ... }: let
   pkg = pkgs.ungoogled-chromium;
 in {
   home.packages = [ pkg ];
   home.file.".local/share/applications/chromium-browser.desktop".source = let
-    flags = "--enable-features=UseOzonePlatform --ozone-platform=wayland --gtk-version=4";
+    imeFlag = if osConfig.services.xserver.desktopManager.gnome.enable
+      then "--gtk-version=4"
+      else "--enable-wayland-ime";
+    flags = "--enable-features=UseOzonePlatform --ozone-platform=wayland ${imeFlag}";
   in pkgs.runCommand "chromium-wayland-ime-desktop" {} ''
     cp ${pkg}/share/applications/chromium-browser.desktop $out
     sed -i 's/Exec=chromium/Exec=chromium ${flags}/g' $out
